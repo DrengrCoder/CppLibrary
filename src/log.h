2a7f3c638f4a1e9e4f97e4e1c8a3f2423afd9159
Dylan McAdam <dylan.mcadam@marineai.co.uk>
2024-03-02 20:46:15 +0000

    Updated base level log macro to avoid conflicting with other libraries

--- src/log.h
+++ src/log.h
@@ -45,7 +45,7 @@ extern class LogSettings LOG_SETTINGS;
 /**
  * clog << "LT_LL_INFO type";
  */
-#define clog Log(LogType::LT_LL_INFO, __FILE__, __LINE__)
+#define llog Log(LogType::LT_LL_INFO, __FILE__, __LINE__)
 /**
  * ilog << "LT_INFO type";
  */
